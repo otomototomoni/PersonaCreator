112e77baa2dade4a8947982cddf8c8fa98c33f9f
otomototomoni <tegurechafu150415@icloud.com>
2024-07-19 14:14:39 +0900

    PersonaEditScreen.kt を変更 EditTextの追加ボタンを押すとそれぞれの枠でEditTextが追加されるように修正した。

--- app/src/main/java/com/example/personacreator/PersonaEditScreen.kt
+++ app/src/main/java/com/example/personacreator/PersonaEditScreen.kt
@@ -17,6 +17,8 @@ class PersonaEditScreen : AppCompatActivity() {
 //行動変数
     private lateinit var inputContainerAction: LinearLayout
     private lateinit var addButtonAction: Button
+
+    //onCreate メイン処理
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_persona_edit_screen)
@@ -26,23 +28,22 @@ class PersonaEditScreen : AppCompatActivity() {
         val backListener = MainBackListener()
         //mainBackBtにリスナを入れる。
         mainBackBt.setOnClickListener(backListener)
-
+        
         //地理的変数のedittext追加処理
         inputContainerGeography = findViewById<LinearLayout>(R.id.input_container_geography)
         addButtonGeography = findViewById<Button>(R.id.add_button_geography)
-
-        addButtonGeography.setOnClickListener {
-            addInputField()
-        }
-
+        addButtonGeography.setOnClickListener{
+            addInputField(inputContainerGeography)
+        }//ボタンにリスナを追加
         //行動変数のEditText追加処理
         inputContainerAction = findViewById<LinearLayout>(R.id.input_container_action)
         addButtonAction = findViewById<Button>(R.id.add_button_action)
         addButtonAction.setOnClickListener{
-            addInputField()
-        }
+            addInputField(inputContainerAction)
+        }//ボタンにリスナを追加
     }
 
+    //戻るボタンを押したときの処理
     private inner class MainBackListener : View.OnClickListener{
         //前の画面に戻る処理
         override fun onClick(v: View?) {
@@ -51,7 +52,8 @@ class PersonaEditScreen : AppCompatActivity() {
 
     }
 
-    private fun addInputField() {
+    //EditTextをそれぞれのLinearLayoutに入れるための処理
+    private fun addInputField(container: LinearLayout) {
         val newInputField = EditText(this).apply {
             layoutParams = LinearLayout.LayoutParams(
                 LinearLayout.LayoutParams.MATCH_PARENT,
@@ -59,15 +61,6 @@ class PersonaEditScreen : AppCompatActivity() {
             )
             hint = "New text"
         }
-        val buttonClickListener = View.OnClickListener { view ->
-        when(view.id) {
-            R.id.add_button_geography -> {
-                inputContainerGeography.addView(newInputField)
-            }
-            R.id.add_button_action -> {
-                inputContainerAction.addView(newInputField)
-            }
-        }
-        }
+        container.addView(newInputField)
     }
 }
